069a5734a62a6fc412072ff64336b6b2db328076
sanyaswee <kozoriz407@gmail.com>
2023-05-09 19:11:11 +0300

    Model Quiz

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Quiz
+
 
 # Register your models here.
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+
+admin.site.register(Quiz, QuizAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Quiz(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    min_age = models.SmallIntegerField(max_length=3)
+    max_age = models.SmallIntegerField(max_length=3)
+    tags = models.TextField()
