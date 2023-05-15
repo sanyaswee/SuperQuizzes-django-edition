577f6766d9df72ba8bed2922afbba661db4dadfa
sanyaswee <kozoriz407@gmail.com>
2023-05-15 19:35:43 +0300

    Show tags

--- home/views.py
+++ home/views.py
@@ -6,4 +6,6 @@ from .models import Quiz
 def index(request):
     quizzes = Quiz.objects.filter(available=True)
 
-    return render(request, 'home/quiz_list.html', {'quizzes': quizzes})
+    color_classes = ['red-box', 'yellow-box', 'blue-box', 'green-box']
+
+    return render(request, 'home/quiz_list.html', {'quizzes': quizzes, 'color_classes': color_classes})
