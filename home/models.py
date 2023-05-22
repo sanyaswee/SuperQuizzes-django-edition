19736dea71fc1a09d250e3a2465ce8a44b7d96f3
sanyaswee <kozoriz407@gmail.com>
2023-05-22 15:17:23 +0300

    Completion and UserAnswers models

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
@@ -21,8 +22,8 @@ class Question(models.Model):
     wrong_answer2 = models.CharField(max_length=100)
     wrong_answer3 = models.CharField(max_length=100)
 
-    answers_amount = models.IntegerField(default=0)
-    correct_answers = models.IntegerField(default=0)
+    # answers_amount = models.IntegerField(default=0)
+    # correct_answers = models.IntegerField(default=0)
 
     def __str__(self):
         return f'#{self.id}: {self.question}'
@@ -35,11 +36,39 @@ class Quiz(models.Model):
     tags = models.ManyToManyField(Tag, related_name='quizzes')
     questions = models.ManyToManyField(Question, related_name='quizzes')
 
-    completed_as_form = models.IntegerField(default=0)
-    completed_as_quiz = models.IntegerField(default=0)
-    average_score = models.IntegerField(default=0)
+    # completed_as_form = models.IntegerField(default=0)
+    # completed_as_quiz = models.IntegerField(default=0)
+    # average_score = models.IntegerField(default=0)
 
     available = models.BooleanField(default=False)
 
     def __str__(self):
         return f'#{self.id}: {self.name}'
+
+
+class Completion(models.Model):  # Instead of 'Quiz.completed_as_...' and 'Quiz.average_score'
+    # General quiz info
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='completions')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='completions', null=True
+    )
+    is_form = models.BooleanField()  # Instead of 'completion_type' filed. True = form, False = quiz
+
+    # Time info
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(auto_now=True)
+
+    # Results info
+    right_answers = models.IntegerField()
+    score = models.IntegerField()
+
+
+class UserAnswer(models.Model):
+    completion = models.ForeignKey(Completion, on_delete=models.CASCADE, related_name='answers')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='user_answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='user_answers')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='quiz_answers', null=True
+    )
+
+    right = models.BooleanField()
