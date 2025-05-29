4c031d091d2219c7b7f5bec742222e6578819a37
sanyaswee <kozoriz407@gmail.com>
2025-05-29 16:59:02 +0300

    Hide secret key + debug

--- SuperQuizzes/settings.py
+++ SuperQuizzes/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=01t5=i(wrso^^7axm3!qpxq$v4v4i&uaemaw0vos_v#ddi+&c'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
